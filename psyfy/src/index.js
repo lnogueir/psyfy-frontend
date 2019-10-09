a6e1aa0627afb990e22356ca1c7698ba944daa4d
Lucas Nogueira <lnogueir@uwaterloo.ca>
2019-10-08 21:46:32 -0400

    commiting agian

--- psyfy/src/index.js
+++ psyfy/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import OverviewPage from './pages/therapist/OverviewPage'
+import OverviewPage from './pages/therapist/OverviewPage';
 import * as serviceWorker from './serviceWorker';
 
 
