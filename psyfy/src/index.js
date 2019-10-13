1e6b85f8e4f1321fdb598eb269975238b4f9d89e
lnogueir <48890798+lnogueir@users.noreply.github.com>
2019-10-13 19:31:24 -0400

    Update index.js

--- psyfy/src/index.js
+++ psyfy/src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
 import OverviewPage from './pages/therapist/OverviewPage';
-=======
-import OverviewPage from './pages/therapist/OverviewPage'
-import TherapistForm from './pages/therapist/TherapistForm'
-import TestPage from './pages/public/LoginCardPage'
->>>>>>> fazendo mudancas no  login
 import * as serviceWorker from './serviceWorker';
 
 
 
 
-ReactDOM.render(<TestPage />, document.getElementById('root'));
+ReactDOM.render(<OverviewPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
