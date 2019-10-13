da182041ee55a893bec8cfd1d568063c49994ffd
Andre <andrenetto@poli.ufrj.br>
2019-10-13 20:13:05 -0300

    Added changes

---
+++ psyfy/src/common_components/LoginOverlay.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+
+function LoginOverlay(props){
+    const childrenWithProps = React.Children.map(props.children, child =>
+      React.cloneElement(child, { toggleShouldDisplay: props.toggleShouldDisplay })
+    );
+    return(
+
+      <div
+        style={{height:props.should_display?"100%":"0%"}}
+        onClick={props.toggleShouldDisplay}
+        className="overlay"
+      >
+        <div onClick={(e)=>e.stopPropagation()} className="overlay-content">
+            {childrenWithProps}
+         </div>
+      </div>
+      
+  )
+}
+
+export default OverviewOverlay;
